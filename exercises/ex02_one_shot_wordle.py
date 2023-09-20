71c8c0f14dd18a2f10f22f91db98b9f8a830c304
Sophia Fisher <safisher@unc.edu>
2023-09-20 19:03:44 -0400

    update to one shot wordle

---
+++ exercises/ex02_one_shot_wordle.py
@@ -0,0 +1,34 @@
+"""Ex01 - Wordle -One try Wordle!"""
+__author__ = "730648328"
+
+"""Establishing variables used in the rest of my program"""
+secret: str = "python"
+len_secret: int = len(secret)
+user_guess: str = input("What is your 6 letter guess? ")
+i: int = 0
+emoji_result: str = ""
+same_letter: bool = False
+find_match: int = 0
+while len(user_guess) != len_secret:
+    user_guess = input(f"That was not { len_secret } letters! Try again? ")
+while i < len_secret:
+    if user_guess[i] == secret[i]:
+        emoji_result = emoji_result + "\U0001F7E9"
+    else:
+        while (same_letter != True) and (find_match < len_secret):
+            if user_guess[i] == secret[find_match]:
+                same_letter = True
+            else:
+                find_match += 1
+        if same_letter == True:
+            emoji_result = emoji_result + "\U0001F7E8"
+        else:
+            emoji_result = emoji_result + "\U00002B1C"
+        same_letter = False
+        find_match = 0
+    i += 1
+print(emoji_result)
+if user_guess == secret:
+    print("Woo! You got it!")
+else:
+    print("Not quite. Play again soon!")
